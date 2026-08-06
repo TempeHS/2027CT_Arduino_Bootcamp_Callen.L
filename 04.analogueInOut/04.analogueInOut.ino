58070d4bdfa4bde72b74c3577be0dab9bfb6074a
Callen :D <callen.lin1@education.nsw.gov.au>
2026-08-06 01:35:18 +0000

    complete: analogueInOuttask

--- 04.analogueInOut/04.analogueInOut.ino
+++ 04.analogueInOut/04.analogueInOut.ino
@@ -42,9 +42,33 @@ const int LED_PIN = 6;     // Grove LED on D6 (PWM)
 const int BUZZER_PIN = 5;  // Grove Buzzer on D5 (PWM)
 
 void setup() {
-
+  Serial.begin(115200);                  // Start talking to the computer at 115200 baud
+  pinMode(LED_PIN, OUTPUT); // add
+  Serial.println("Arduino ready!");      // When does this line print?
+  Serial.println(7 / 2);                 // prediction: ?
+  Serial.println(7.0 / 2);               // prediction: ?
+  Serial.println(2 + 3 * 4);             // prediction: ?
+  Serial.println((2 + 3) * 4);           // prediction: ?
+  Serial.println(7 % 3);                 // prediction:
 }
 
 void loop() {
-
+  digitalWrite(LED_PIN, HIGH);  // 5 volts on the pin: LED on
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);   // 0 volts: LED off
+  delay(1000);
+  analogWrite(LED_PIN, 0);
+  delay(1000);
+  analogWrite(LED_PIN, 64);    // 25 percent duty
+  delay(1000);
+  analogWrite(LED_PIN, 127);   // 50 percent duty
+  delay(1000);
+  analogWrite(LED_PIN, 255);   // always on
+  delay(1000);
+  tone(BUZZER_PIN, 262);   // middle C
+  delay(250);
+  tone(BUZZER_PIN, 523);   // C an octave up
+  delay(250);
+  noTone(BUZZER_PIN);      // silence
+  delay(100000000000000000000000000);
 }
