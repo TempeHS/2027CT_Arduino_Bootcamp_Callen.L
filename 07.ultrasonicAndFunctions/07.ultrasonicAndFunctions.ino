848aaa023291eae661764c503581a0666c1b84fa
Callen :D <callen.lin1@education.nsw.gov.au>
2026-08-20 02:30:17 +0000

    complete 11.3 boomGates or... rams.

--- 07.ultrasonicAndFunctions/07.ultrasonicAndFunctions.ino
+++ 07.ultrasonicAndFunctions/07.ultrasonicAndFunctions.ino
@@ -1,57 +1,31 @@
 #include "Ultrasonic.h"
-#include <Servo.h>
 
-const int BUZZER_PIN = 5;
 
-const int LED_PIN = 6;
+Ultrasonic ultrasonic(2);   // Grove 3-pin sensor: trigger and echo share D2
 
-const int SERVO_PIN = 3;
+void setup() {
+  Serial.begin(115200);
+}
 
-Ultrasonic ultrasonic(2);
-
-Servo boomGate;
 
 int readDistance() {
   return ultrasonic.read();
 }
 
 int classifyZone(int distance, int nearLimit, int farLimit) {
-
   if (distance < nearLimit) {
-    return 0;
+    return 0;              // danger
   } else if (distance < farLimit) {
-    return 1;
+    return 1;              // warning
   }
-
-  return 2;
-}
-
-void setup() {
-
-  Serial.begin(115200);
-
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUZZER_PIN, OUTPUT);
-
-  boomGate.attach(SERVO_PIN);
-  boomGate.write(90);
+  return 2;                // safe
 }
 
 void loop() {
-
   int distance = readDistance();
-
   int zone = classifyZone(distance, 10, 30);
-
   Serial.print(distance);
   Serial.print(" cm, zone ");
   Serial.println(zone);
-
-  if (distance > 0 && distance < 30) {
-    boomGate.write(180);
-  } else {
-    boomGate.write(90);
-  }
-
   delay(100);
 }
