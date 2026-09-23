d2b921c78f0a340470a6b6dc3290522667ee3347
Callen :D <callen.lin1@education.nsw.gov.au>
2026-09-23 14:42:38 +0000

    feat(11.1): code and image finished!

--- 11.myProjects/11.1.lightSwitch/11.1.lightSwitch.ino
+++ 11.myProjects/11.1.lightSwitch/11.1.lightSwitch.ino
@@ -1,31 +1,5 @@
-
-const int POT_PIN = A0;
-const int BUZZER_PIN = A2;  // Grove Sound sensor on A2
-const int LED_PIN = 6;
-
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-}
-
-void loop() {
-  int potValue = analogRead(POT_PIN);
-
-  int LED_VALUE = map(potValue, 0, 1023, 0, 255);
-
-  if (LED_VALUE == 128) {
-    analogWrite(LED_PIN, 200);
-    noTone(BUZZER_PIN);
-  }
-  else {
-    analogWrite(LED_PIN, 0);
-    tone(BUZZER_PIN, 1000);
-  }
-
-  delay(10);
-}
-
 /*
-  Author:
+  Author: Callen Lin
   Learning Intention: Students will use a range of simple sensors to control an LED UI
 
   Student Notes:
@@ -34,3 +8,32 @@ void loop() {
 
   3. Suggested Grove ports: Button D4, Potentiometer A0, LED D6, Light sensor A3
 */
+
+const int POT_PIN = A0; // pot (yes pot, not pot., that is the wrong pot)
+const int LED_PIN = 6;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int potValue = analogRead(POT_PIN);
+
+  if (potValue < 481) { // where'd i get the number? i threw blindly typed three number and then had to delete a ridiculous a that sneaked in.
+    analogWrite(LED_PIN, 0);
+    Serial.println("Light: OFF");
+  }
+
+  else if (potValue < 783) { // where did this number come from. used a pen and chose randomly.
+    analogWrite(LED_PIN, 100);
+    Serial.println("Light: Pretty DIM");
+  }
+
+  else {
+    analogWrite(LED_PIN, 255);
+    Serial.println("Light: FULL");
+  }
+
+  delay(50);
+}
