fc1229b939beb0da8888e65bd90944e0e34cc9fb
Callen :D <callen.lin1@education.nsw.gov.au>
2026-09-23 12:19:50 +0000

    refactor: 01 and 02 move comments to top and adjusted

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,6 +1,34 @@
 /*
   Author: Callen Lin
 
+  Learning Intention:
+  The students will understand the difference between the inbuilt setup and loop methods,
+  and use the Serial Monitor as a debugging window into a running program.
+
+  Success Criteria:
+    1.  I understand the different behaviours of the setup and loop methods
+    2.  I can print information to the serial monitor
+    3.  I can print information to a new line of the serial monitor
+    4.  I understand the impact of having more than one setup or loop method in my sketch
+    5.  I understand single-line and multi-line comments
+    6.  I can recognise a missing-semicolon compiler error
+
+  The Rules of Every Sketch:
+    1. The .ino file name must match its folder name
+    2. Exactly ONE setup() { } function
+    3. Exactly ONE loop() { } function
+    4. Every function needs BOTH braces
+    5. Every statement ends with a semicolon ;
+
+  Hardware:
+    None needed. Just your Arduino, the USB cable and the Serial Monitor at 115200 baud.
+
+  Documentation:
+    https://www.arduino.cc/reference/en/language/functions/communication/serial/
+    https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
+*/
+
+/*
   The program below setups the program and starts counting up by 1 every 1.5 seconds.
 
   The sketch above is a refined version of the original.
@@ -51,36 +79,6 @@ void loop() {
   delay(1500); // Loop every 1.5s
 
 }
-
-/*
-  Author: Callen Lin
-
-  Learning Intention:
-  The students will understand the difference between the inbuilt setup and loop methods,
-  and use the Serial Monitor as a debugging window into a running program.
-
-  Success Criteria:
-    1.  I understand the different behaviours of the setup and loop methods
-    2.  I can print information to the serial monitor
-    3.  I can print information to a new line of the serial monitor
-    4.  I understand the impact of having more than one setup or loop method in my sketch
-    5.  I understand single-line and multi-line comments
-    6.  I can recognise a missing-semicolon compiler error
-
-  The Rules of Every Sketch:
-    1. The .ino file name must match its folder name
-    2. Exactly ONE setup() { } function
-    3. Exactly ONE loop() { } function
-    4. Every function needs BOTH braces
-    5. Every statement ends with a semicolon ;
-
-  Hardware:
-    None needed. Just your Arduino, the USB cable and the Serial Monitor at 115200 baud.
-
-  Documentation:
-    https://www.arduino.cc/reference/en/language/functions/communication/serial/
-    https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
-*/
 
 /*
   Goals of the lesson:
--- 02.storingData/02.storingData.ino
+++ 02.storingData/02.storingData.ino
@@ -1,6 +1,27 @@
 /*
   Author: Callen Lin
 
+  Learning Intention:
+  The students will learn the different data types used in the C++ language,
+  watch an integer overflow live, and understand variable scope.
+
+  Success Criteria:
+    1.  I understand and can store variables in the datatypes String, int, float, long and bool
+    2.  I know how to access more information about the different datatypes in the documentation
+    3.  I understand how (1, HIGH and true) and (0, LOW and false) all represent the same concept
+    4.  I can convert an int, float and bool to a String and print it to the serial monitor
+    5.  I understand the difference between an int and an unsigned int, and a long and an unsigned long
+    6.  I understand the difference between a global variable and a function (local) variable
+
+  Hardware:
+    None needed. Serial Monitor at 115200 baud.
+
+  Documentation:
+    https://www.arduino.cc/reference/en/#variables
+    https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
+*/
+
+/*
   The program below picks 2 random numbers and will continue rolling random numbers until both numbers matches.
   There is also a secret number of ten where hitting it gives a custom notification.
 
@@ -124,42 +145,11 @@ void loop() {
 
 }
 
-
-
-
-
-
-
-
-
 /*
   The sketch above is a refined version of the original.
 
   Containing a few white spaces for readability purposes and better structure :P
   I personally use camelCase over snake_case since I code quite frequently in Swift.
-*/
-
-/*
-  Author: Callen Lin
-
-  Learning Intention:
-  The students will learn the different data types used in the C++ language,
-  watch an integer overflow live, and understand variable scope.
-
-  Success Criteria:
-    1.  I understand and can store variables in the datatypes String, int, float, long and bool
-    2.  I know how to access more information about the different datatypes in the documentation
-    3.  I understand how (1, HIGH and true) and (0, LOW and false) all represent the same concept
-    4.  I can convert an int, float and bool to a String and print it to the serial monitor
-    5.  I understand the difference between an int and an unsigned int, and a long and an unsigned long
-    6.  I understand the difference between a global variable and a function (local) variable
-
-  Hardware:
-    None needed. Serial Monitor at 115200 baud.
-
-  Documentation:
-    https://www.arduino.cc/reference/en/#variables
-    https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
 */
 
 /*
